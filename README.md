de594eec816cd5a14d6f4f42fa2289b3bb31b74f
simon-p-r <simon.ricaldone@circabs.com>
2015-06-08 13:31:07 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,81 @@
+#xl-json
+
+Converts a complete excel spreadsheet to json and writes file out, optionally it can pipe one worksheet json stream to a pipeline  
+
+<!-- [![Build Status](https://travis-ci.org/simon-p-r/find-key.svg?branch=master)](https://travis-ci.org/simon-p-r/find-key)
+[![Current Version](https://img.shields.io/npm/v/find-key.svg)](https://www.npmjs.org/package/find-key)
+![devDependencies](http://img.shields.io/david/dev/simon-p-r/find-key.svg) -->
+
+
+### Install
+
+````ShellSession
+
+npm install xl-json
+
+````
+
+### Example
+
+```js
+
+var Converter = require('xl-json');
+
+var formatter = function (arr) {
+
+    var results = [];
+    var obj = {};
+
+    arr.map(function (type) {
+
+        var sub = Object.keys(type);
+
+        for (var i = 0; i < sub.length; i++) {
+            var key = sub[i];
+            var value = type[key];
+            if (key.indexOf(':') !== -1) {
+                var keys = key.split(':');
+                var collection = keys[1];
+                var name = keys[0];
+                obj[name] = {
+                    cn: collection,
+                    q: {
+                        sid: value
+                    }
+                };
+                results.push(obj);
+
+            } else {
+                obj[key] = value;
+                results.push(obj[key]);
+            }
+        }
+    });
+    return results;
+
+};
+
+var options = {
+    input: Path.resolve(__dirname, './test/excel/relationship.xlsx'),
+    output: '../output/relationship',
+    spacer: 2, // optional for JSON.stringify
+    replacer: ['key'] // optional array or function to be passed to JSON.stringify
+    formatter: formatter // optional to replace standard formatter
+
+};
+
+var results = new Converter(options);
+
+// module will write files out when run to output directory
+
+results.cursor('customer').pipe(process.stdout);
+
+// Also exposes a stream which can be written of piped somewhere
+
+```
+
+###Todo
+
++ Separate file writing into own routine, plus option to write one sheet and multiple spreadsheets
++ Add travis for testing and improve testing of streams
++ Stream not just per sheet but per file or directory of files
---
+++ test/index.js
@@ -0,0 +1,126 @@
+// Load modules
+
+var Code = require('code');
+var Lab = require('lab');
+var Path = require('path');
+var Readable = require('stream').Readable;
+var Converter = require('../lib/index.js');
+var StdMocks = require('std-mocks');
+
+
+// Set-up lab
+var lab = exports.lab = Lab.script();
+var describe = lab.describe;
+var it = lab.it;
+var expect = Code.expect;
+
+
+describe('Convert', function () {
+
+
+    it('should throw if not constructed with new', function (done) {
+
+
+        var fn = function () {
+
+            var convert = Converter();
+        };
+
+        expect(fn).throws(Error, 'XlJson must be constructed using new');
+        done();
+
+    });
+
+    it('should throw if options is not an object', function (done) {
+
+        var fn = function () {
+
+            var convert = new Converter();
+        };
+
+        expect(fn).throws(Error, 'XlJson must be constructed with an options object');
+        done();
+
+    });
+
+    it('should throw if input file doesn\t exist', function (done) {
+
+        var fn = function () {
+
+            var convert = new Converter({
+                input: Path.resolve(__dirname, '../test/excel/relationship.xls')
+            });
+        };
+
+        expect(fn).throws(Error);
+        done();
+
+    });
+
+    it('should throw if output directory doesn\t exist', function (done) {
+
+        var fn = function () {
+
+            var convert = new Converter({
+                input: Path.resolve(__dirname, '../test/excel/relationship.xls'),
+                output: 'invalid'
+            });
+        };
+
+        expect(fn).throws(Error);
+        done();
+
+    });
+
+    it('should have settings defined', function (done) {
+
+        var parser = new Converter({
+            input: Path.resolve(__dirname, '../test/excel/relationship.xlsx'),
+            output: Path.resolve(__dirname, '../output/relationship'),
+            spacer: 2
+
+        });
+
+        expect(parser.settings).to.be.an.object();
+        done();
+
+    });
+
+    it('should have data object defined', function (done) {
+
+        var parser = new Converter({
+            input: '../test/excel/relationship.xlsx',
+            output: '../output/relationship',
+            spacer: 4
+
+        });
+
+        expect(parser.data).to.be.an.object();
+        expect(parser.settings).to.be.an.object();
+        expect(parser.settings.spacer).to.equal(4);
+        done();
+
+    });
+
+    it('should have expose a cursor stream', function (done) {
+
+        var parser = new Converter({
+            input: '../test/excel/relationship.xlsx',
+            output: '../output/relationship',
+            spacer: 4
+
+        });
+
+        StdMocks.use();
+        parser.cursor('employment').pipe(process.stdout);
+        StdMocks.restore();
+        var test = StdMocks.flush();
+        expect(test.stdout[0]).to.equal(test.stdout[1]);
+        done();
+
+    });
+
+
+
+
+});
